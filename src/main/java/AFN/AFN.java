67414e0571b00f30dfa6ff1b390d6ba21044e5d6
lucaslimacodes <lucaslima12345.41@gmail.com>
2024-12-13 18:39:42 -0300

    Epsilon implemented.
    Lacks tests tho

--- src/main/java/AFN/AFN.java
+++ src/main/java/AFN/AFN.java
@@ -7,13 +7,25 @@ import main.java.Util.Pair;
 import java.util.ArrayList;
 
 public class AFN extends Automata {
-    public static Character EPSLON = '$';
+    public static Character EPSILON = '$';
     private ArrayList<String> currState;
 
     public AFN(char[] alphabet, String[] allStates, String[] acceptanceState, String initialState) {
         super(alphabet, allStates, acceptanceState, initialState);
         this.currState = new ArrayList<>();
         this.currState.add(initialState);
+    }
+
+    private ArrayList<String> allStatesFromEpsilon(String beginState){
+        State state = getStateByID(beginState);
+        ArrayList<String> ret = new ArrayList<>();
+        for(Pair<Character,String> transition : state.getTransitions()){
+            if(transition.getFirst().equals(EPSILON)){
+                ret.add(transition.getSecond());
+                ret.addAll(allStatesFromEpsilon(transition.getSecond()));
+            }
+        }
+        return ret;
     }
 
     protected void feed(char symbol){
@@ -23,6 +35,7 @@ public class AFN extends Automata {
             for(Pair<Character,String> transition : state.getTransitions()){
                 if(transition.getFirst() == symbol){
                     newStates.add(transition.getSecond());
+                    newStates.addAll(allStatesFromEpsilon(transition.getSecond()));
                 }
             }
         }
@@ -36,6 +49,7 @@ public class AFN extends Automata {
     }
 
     public boolean accepts(String word){
+        this.currState.addAll(allStatesFromEpsilon(this.currState.getFirst()));
         for(char c : word.toCharArray()){
             feed(c);
         }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,24 +8,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         char[] alphabet = {'0','1'};
-        String[] allStates = {"q1", "q2", "q3", "q4"};
-        String[] accept = {"q4"};
-        AFN afn = new AFN(alphabet, allStates, accept, "q1");
-        afn.addTransition("q1", "q1", '0');
-        afn.addTransition("q1", "q1", '1');
-        afn.addTransition("q1", "q2", '1');
-        afn.addTransition("q2", "q3", '0');
-        afn.addTransition("q2", "q3", '1');
-        afn.addTransition("q3", "q4", '0');
-        afn.addTransition("q3", "q4", '1');
-        System.out.println(afn.accepts("10000000000"));
-
+        String[] allStates = {"1", "2", "3"};
+        String[] accept = {"3"};
+        AFN afn = new AFN(alphabet, allStates, accept, "1");
+        afn.addTransition("1" ,"1", '0');
+        afn.addTransition("1" ,"2", AFN.EPSILON);
+        afn.addTransition("2" ,"2", '1');
+        afn.addTransition("2" ,"3", '0');
+        afn.addTransition("3" ,"3", '0');
 //        AFD afd = new AFD(alphabet, allStates, accept, "q1");
 //        afd.addTransition("q1","q1", '1');
 //        afd.addTransition("q1","q2", '0');
 //        afd.addTransition("q2","q2", '1');
 //        afd.addTransition("q2","q1", '0');
 //        System.out.println(afd.accepts("0101100111111111011"));
+        System.out.println(afn.accepts("00001110"));
+
+
 
     }
 }
